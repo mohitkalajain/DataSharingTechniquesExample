20b6fb8af5c8ce1f86b2c78d2fe926effaa68b27
Mohit Kala <73868188+mohitkalajain@users.noreply.github.com>
2022-08-27 10:21:20 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using DataSharingTechniquesExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataSharingTechniquesExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "MVC";
+           
+            return View();
+        }
+        public IActionResult ViewBagIndex()
+        {
+            ViewBag.Name = "MVC";
+            return View();
+        }
+        public ActionResult TempDataIndex()
+        {
+            TempData["Name"] = "MVC";
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
